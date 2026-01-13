231ccaa68e0b8580b817d8029154d98be228cd8f
relatable52 <duongchinh29062003@gmail.com>
2026-01-13 14:07:58 +0700

    Add startup script

--- startup.sh
+++ startup.sh
@@ -2,6 +2,4 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 uv sync
 
-./.venv/Scripts/activate
-
-gunicorn --workers 4 --threads 2 --worker-class gthread --timeout 1000 -b 0.0.0.0:8050 main:server
+uv run gunicorn --workers 4 --threads 2 --worker-class gthread --timeout 1000 -b 0.0.0.0:8050 main:server
